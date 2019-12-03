6cfbc7993d89340df2a00a56bcf661fbdc09a365
justin cechmanek <gj.cechmanek@gmail.com>
2019-12-02 21:06:42 -0800

    adds the input output module

---
+++ src/io/compress.rs
@@ -0,0 +1,12 @@
+// implements the IO compression and uncompression submodule
+
+pub enum Method {
+  jpg,
+  png,
+  gif,
+  raw,
+}
+
+pub fn compress(/*image*/ method: Method, file_name: &str) {
+
+}
---
+++ src/io/read_write.rs
@@ -0,0 +1,28 @@
+// implements the IO read and write submodule
+
+pub fn load_image(file_name: &str) /*-> Image*/ {
+
+}
+
+pub fn save_image(/*image: &Image*/file_name: &str) {
+
+}
+
+// TODO: how to best read and write video files?
+
+
+pub fn load_camera(file_name: &str) /* -> Camera */ {
+
+}
+
+pub fn save_camera(/*camera: &Camera*/ file_name: &str) {
+
+}
+
+pub fn load_features(file_name: &str) /* -> [Features] */ {
+
+}
+
+pub fn save_features(/*features: &[Features]*/file_name: &str) /* -> [Features] */ {
+
+}
