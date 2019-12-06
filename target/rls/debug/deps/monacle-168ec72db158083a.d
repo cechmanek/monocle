10397f4d093e135de19b9f2c966b360a4449e700
justin cechmanek <gj.cechmanek@gmail.com>
2019-12-05 22:17:03 -0800

    removes build directory

--- target/rls/debug/deps/monacle-168ec72db158083a.d
+++
@@ -1,5 +0,0 @@
-d:\Projects\monacle\target\rls\debug\deps\monacle-168ec72db158083a.rmeta: src\main.rs
-
-d:\Projects\monacle\target\rls\debug\deps\monacle-168ec72db158083a.d: src\main.rs
-
-src\main.rs:
--- target/rls/debug/deps/monacle-c5c7bde1e932b041.d
+++
@@ -1,5 +0,0 @@
-d:\Projects\monacle\target\rls\debug\deps\monacle-c5c7bde1e932b041.rmeta: src\main.rs
-
-d:\Projects\monacle\target\rls\debug\deps\monacle-c5c7bde1e932b041.d: src\main.rs
-
-src\main.rs:
